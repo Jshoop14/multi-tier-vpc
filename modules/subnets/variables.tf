8fafa6439e1e52a43bf10a4df3c3b1f5c1c847b9
jshoop14 <jeremy.shoop@comcast.net>
2024-12-14 13:55:23 -0600

    Refactor subnets module to explicitly define variable types

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -1,24 +1,31 @@
 variable "vpc_id" {
   description = "The ID of the VPC"
+  type        = string
 }
 
 variable "public_cidr" {
   description = "CIDR block for the public subnet"
+  type        = string
 }
 
 variable "private_cidr" {
   description = "CIDR block for the private subnet"
+  type        = string
 }
 
 variable "availability_zone" {
   description = "The availability zone for the subnets"
+  type        = string
 }
 
 variable "public_name" {
   description = "Name tag for the public subnet"
+  type        = string
 }
 
 variable "private_name" {
   description = "Name tag for the private subnet"
+  type        = string
 }
+
 
