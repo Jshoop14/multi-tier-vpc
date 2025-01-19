a092fd4cab64622805e624bed135560a42bd8c20
jshoop14 <jeremy.shoop@comcast.net>
2025-01-19 14:29:03 -0600

    Updated SG for the DB SG

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -31,3 +31,30 @@ resource "aws_security_group" "web_sg" {
     Name = var.web_sg_name
   }
 }
+
+resource "aws_security_group" "db_sg" {
+  name        = "DatabaseSecurityGroup"
+  description = "Allow access to the database"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow MySQL from web servers"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "DatabaseSecurityGroup"
+  }
+}
+
